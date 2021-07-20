64be52c8346bc47f6fd2272502654c9eb75346fd
Fai <chepngetichfaith46@gmail.com>
2021-07-20 21:19:29 +0300

    add class project and profile to the models file

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields import TextField
+
 
 # Create your models here.
+
+class Project(models.Model):
+    title=models.CharField(max_length=100,null=False)
+    description=models.TextField(max_length=500)
+    screenshot1=models.ImageField(default='default\.png',blank=True)
+    screenshot2=models.ImageField(default='default\.png',blank=True)
+    screenshot3=models.ImageField(default='default\.png',blank=True)
+    link=models.CharField(max_length=100,blank=True)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+class Profile(models.Model):
+    avatar = models.ImageField(upload_to='avatars/')
+    bio = TextField(max_length=500,null=False)
+    username = models.ForeignKey(User,on_delete=models.CASCADE)
+    name =models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
+
+
+
