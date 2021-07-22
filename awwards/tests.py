0306f5af304833b467586bc0a7586ac112352f06
Fai <chepngetichfaith46@gmail.com>
2021-07-22 11:48:42 +0300

    add test to instantiate tests project

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Profile,Project
 
 # Create your tests here.
+#setup method
+def setUp(self):
+    self.projects=Project(title='mytitle',description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
