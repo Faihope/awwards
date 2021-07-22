9ad5eb0c3d2a593cf77073c4b7974068f38ba95f
Fai <chepngetichfaith46@gmail.com>
2021-07-22 10:32:07 +0300

    Add search functionality to the search button

--- awwards/views.py
+++ awwards/views.py
@@ -12,6 +12,14 @@ from django.core.exceptions import ObjectDoesNotExist
 def index(request):
     projects=Project.objects.all()
 
+    item_name=request.GET.get('item_name')
+    if item_name!='' and item_name is not None:
+        projects=projects.filter(title__icontains=item_name)
+
+    else:
+        messages.info(request, 'No search term found')
+
+
     return render(request,'index.html',{'projects':projects})
 
 def registeruser(request):
