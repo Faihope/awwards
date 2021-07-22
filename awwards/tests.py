e38ce773a2adf752552dfa9c193bafabef373fd7
Fai <chepngetichfaith46@gmail.com>
2021-07-22 12:02:55 +0300

    Add a test case to test the save project method

--- awwards/tests.py
+++ awwards/tests.py
@@ -5,3 +5,12 @@ from .models import Profile,Project
 #setup method
 def setUp(self):
     self.projects=Project(title='mytitle',description='description',screenshot1='s1',screenshot2='s2',screenshot3='s3',link='link')
+
+ #Testing Instance
+def test_instance(self):
+    self.assertTrue(isinstance(self.projects,Project))
+#save method
+def test_save_project(self):
+    self.projects.save_project()
+    projects=Project.objects.all()
+    self.assertTrue(len(projects)>0) 
